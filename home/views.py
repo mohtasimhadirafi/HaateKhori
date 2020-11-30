e393711a5a802fb3c9e8e5595cf3bd403910e004
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2020-11-30 16:10:25 +0600

    linking up going on

--- home/views.py
+++ home/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib import messages
 
 
 def index(request):
@@ -15,48 +14,53 @@ def home(request):
 
 
 def writing(request):
-    return render(request, 'writing.html')
+    return render(request, 'writing_level0.html')
 
 
 def infouser(request):
-    return render(request, 'info_user.html')
+    return render(request,'info_user.html')
 
 
 def surroundings(request):
-    return render(request, 'know_your_surroundings.html')
+    return render(request,'know_your_surroundings.html')
 
 
 def know_you(request):
-    return render(request, 'know_you.html')
+    return render(request,'know_you.html')
 
 
 def my_profile(request):
-    return render(request, 'my_profile.html')
+    return render(request,'my_profile.html')
 
 
 def contact_us(request):
-    return render(request, 'contact_us.html')
+    return render(request,'contact_us.html')
 
 
 def settings(request):
-    return render(request, 'settings.html')
+    return render(request,'settings.html')
 
 
 def add_resource(request):
-    return render(request, 'add_resource.html')
-
+    return render(request,'add_resource.html')
 
 def help(request):
-    return render(request, 'help.html')
-
+    return render(request,'help.html')
 
 def surroundings_shape_level0_identify(request):
-    return render(request, 'surroundings_shape_level0_identify.html')
-
+    return render(request,'surroundings_shape_level0_identify.html')
 
 def shape_level1_learn(requset):
-    return render(requset, 'shape_level1_learn.html')
-
+    return render(requset,'shape_level1_learn.html')
 
 def object_level0_learn(request):
-    return render(request, 'object_level0_learn.html')
+    return render(request,'object_level0_learn.html')
+
+def english_number_level0(request):
+    return render(request, 'english_number_level0.html')
+
+def bangla_charecter_level0(request):
+    return render(request, 'bangla_charecter_level0.html')
+
+def bangla_number_level0(request):
+    return render(request, 'bangla_number_level0.html')
