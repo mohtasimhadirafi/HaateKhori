5511793528435c1354182158066bfb1b430407f6
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2020-12-23 23:15:18 +0600

    come back

---
+++ static/js/change_image_object_learn.js
@@ -0,0 +1,51 @@
+let img = document.querySelector('#shape-back');
+let btnCngLearnNext = document.querySelector('#learn-shape-next-btn');
+let btnCngLearnBack = document.querySelector('#learn-shape-back-btn');
+var shapeImageLearnCount =0;
+
+btnCngLearnBack.style.visibility = 'hidden';
+
+btnCngLearnNext.addEventListener('click' , ()=>{
+    if ( shapeImageLearnCount == 0) {
+        img.src = '/static/img/object_learn_chair.png';
+        shapeImageLearnCount++;
+        btnCngLearnBack.style.visibility = 'visible';
+    }
+    else if(shapeImageLearnCount == 1){
+        img.src = '/static/img/object_learn_laptop_illustartion.png';
+        shapeImageLearnCount++;
+
+    }
+    else if(shapeImageLearnCount == 2){
+        img.src = '/static/img/shape_quadrangle_learn.png';
+        shapeImageLearnCount++;
+    }
+    else if(shapeImageLearnCount == 3){
+        img.src = '/static/img/shape_together1_learn.png';
+
+        btnCngLearnNext.style.visibility = 'hidden';
+    }
+
+})
+
+btnCngLearnBack.addEventListener('click' , ()=>{
+    if ( shapeImageLearnCount == 3) {
+        img.src = '/static/img/shape_quadrangle_learn.png';
+        shapeImageLearnCount--;
+
+    }
+    else if(shapeImageLearnCount == 2){
+        img.src = '/static/img/shape_circle_learn.png';
+        shapeImageLearnCount--;
+
+    }
+    else if(shapeImageLearnCount == 1){
+        img.src = '/static/img/shape_triangle_learn.png';
+        shapeImageLearnCount--;
+    }
+    else if(shapeImageLearnCount == 0){
+        img.src = '/static/img/shape_line_img_background.png';
+        btnCngLearnBack.style.visibility = 'hidden';
+        btnCngLearnNext.style.visibility = 'visible';
+    }
+})
