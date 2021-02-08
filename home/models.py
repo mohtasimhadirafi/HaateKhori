42fef37dd6641153421a2e797d928f85ec2182ff
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-08 10:02:01 +0600

    image save from django

--- home/models.py
+++ home/models.py
@@ -1,3 +1,46 @@
+from django.core.files.base import ContentFile
 from django.db import models
+from .objectDetect import work
+from PIL import Image
+import numpy as np
+from io import BytesIO
+
 
 # Create your models here.
+class UploadObjectDetection(models.Model):
+    image = models.ImageField(upload_to='objectDetectImages')
+
+    def __str__(self):
+        return str(self.id)
+
+    def save(self, *args, **kwargs):
+
+        #open image
+        pil_img = Image.open(self.image)
+
+
+
+        #some processing
+        cv_img = np.array(pil_img)
+        img = work(cv_img)
+
+        print('Image3')
+        print(img)
+
+        #covert back to pil image
+        im_pil = Image.fromarray(img)
+
+        #save
+        buffer = BytesIO()
+        im_pil.save(buffer, format='png')
+
+        image_png = buffer.getvalue()
+
+        self.image.save(str(self.image), ContentFile(image_png),save = False)
+
+        super().save(*args, **kwargs)
+
+
+
+
+
---
+++ home/objectDetect.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+from _collections import defaultdict
+
+# Load Yolo
+labels = []
+myDict = defaultdict(list)
+functionDict = defaultdict(list)
+
+def work(img):
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("coco.names","r") as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    layer_names = net.getLayerNames()
+    outputLayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes)+2, 3))
+
+    # with output layer can get the final detection of the object
+
+    # Loading image
+
+    img = cv2.resize(img,None, fx=.3, fy=.3)
+    height, width, channels = img.shape
+
+
+    # detetcting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    # for b in blob:
+    #     for n, img_blob in enumerate(b):
+    #         cv2.imshow(str(n), img_blob)
+
+    net.setInput(blob)
+    outs = net.forward(outputLayers)
+
+
+    # Showing information on the screen
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:          #object detetcted
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                #cv2.circle(img, (center_x, center_y), 5, (0, 255, 0), 2)
+
+                # Rectangle co ordinates
+
+                x = int(center_x-w/2)
+                y = int(center_y - h/2)
+
+                #cv2.rectangle(img, (x, y), ( x+w, y+h), (0, 255, 0), 1)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+
+
+
+    #print(len(boxes))
+    #number_objects_detected = len(boxes)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    #print(indexes)
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    j = 1
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            #print(label)
+            list = []
+            list.append(x)
+            list.append(y)
+            list.append(w)
+            list.append(h)
+
+            myDict[label].append(list)
+
+
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y +h), color, 1)
+            cv2.putText(img, label, (x, y+20), font, 1, color, 2)
+
+
+    return img,myDict
+
+
+
+
+
